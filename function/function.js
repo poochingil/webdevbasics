758b0ee9df64fccfb4b42b0dd3f19140acf2a2a6
poochingil <siddhantgreat24@gmail.com>
2020-09-21 23:07:58 +0530

    done

---
+++ function/function.js
@@ -0,0 +1,5 @@
+function greet(){
+    console.log('hello'+''+name);
+}
+name='sonu';
+greet();
---
+++ object/object.js
@@ -0,0 +1,5 @@
+let person={
+    name : 'poochi',
+    age : '20'
+};
+console.log(person.name+'hello')
